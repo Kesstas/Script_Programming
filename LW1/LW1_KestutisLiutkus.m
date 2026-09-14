e798ba5507314534c324bde22279661ca1c1c5ac
Kesstas <kestutisliutkuslt@gmail.com>
2026-09-14 12:56:04 +0300

    Update LW1_KestutisLiutkus.m

--- LW1/LW1_KestutisLiutkus.m
+++ LW1/LW1_KestutisLiutkus.m
@@ -10,3 +10,25 @@ plot(x, y, 'o-r', x, y/3, 'xb')
 title('Dvi funkcijos')
 xlabel('X-ai')
 ylabel('F_1 [-o-]    |    F_2 [-x-]')
+
+%%
+help sin
+doc sin
+%%
+help plot
+doc plot
+%%
+help title
+doc title
+%%
+help linspace
+doc linspace
+%%
+help size
+doc size
+%%
+help max
+doc max
+%%
+
+
